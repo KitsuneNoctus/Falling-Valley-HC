fd53a4b11a3f2bbe4ac6b1f7d8e629c6eacbcd8f
Henry Calderon <theceintiant@gmail.com>
2021-03-15 21:36:32 -0700

    Setting Up Files

---
+++ Falling Valley/Falling Valley/Scenes/GameOverScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameOverScene.swift
+//  Falling Valley
+//
+//  Created by Henry Calderon on 3/15/21.
+//
+
+import SpriteKit
+
+
+class GameOverScene: SKScene {
+    
+    override init(size: CGSize) {
+        // do initial configuration work here
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Falling Valley/Falling Valley/View Controllers/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Falling Valley
+//
+//  Created by Henry Calderon on 3/15/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
