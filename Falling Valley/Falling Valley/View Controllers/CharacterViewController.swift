c1e5b3cbc8e610c39a5e8010acd519c16d4d6bd7
Henry Calderon <theceintiant@gmail.com>
2021-03-15 23:01:04 -0700

    Started View controller

---
+++ Falling Valley/Falling Valley/View Controllers/CharacterViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CharacterViewController.swift
+//  Falling Valley
+//
+//  Created by Henry Calderon on 3/15/21.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+    /// To be implemented
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- Falling Valley/Falling Valley/View Controllers/HomeViewController.swift
+++ Falling Valley/Falling Valley/View Controllers/HomeViewController.swift
@@ -8,11 +8,57 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Falling Valley"
+        return label
+    }()
+    
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let characterButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let settingsButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let vStack: UIStackView = {
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.spacing = 20
+        stack.axis = .vertical
+        return stack
+    }()
+    
+    //MARK: View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupView()
     }
-
+    
+    //MARK: Setup
+    func setupView(){
+        self.view.addSubview(titleLabel)
+        self.view.addSubview(vStack)
+        self.vStack.addArrangedSubview(playButton)
+        self.vStack.addArrangedSubview(characterButton)
+        self.vStack.addArrangedSubview(settingsButton)
+        
+        NSLayoutConstraint.activate([
+            
+        ])
+    }
+    
 }
