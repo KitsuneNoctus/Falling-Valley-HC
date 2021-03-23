207106b4e5321fa9319b39a6df83a5b19924e7ec
Henry Calderon <theceintiant@gmail.com>
2021-03-23 11:37:23 -0700

    Would like to try and add persistence and game center

---
+++ Falling Valley/Falling Valley/Persistence/RWGameData.h
@@ -0,0 +1,36 @@
+//
+//  RWGameData.h
+//  Falling Valley
+//
+//  Created by Henry Calderon on 3/23/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RWGameData : NSObject
+
+@property (assign, nonatomic) long score;
+
+@property (assign, nonatomic) long highScore;
+
++(instancetype)sharedGameData;
+-(void)reset;
+
+@end
+
+//+ (instancetype)sharedGameData {
+//    static id sharedInstance = nil;
+//    
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        sharedInstance = [[self alloc] init];
+//    });
+//    
+//    return sharedInstance;
+//}
+
+NS_ASSUME_NONNULL_END
+
+
---
+++ Falling Valley/Falling Valley/Persistence/RWGameData.m
@@ -0,0 +1,12 @@
+//
+//  RWGameData.m
+//  Falling Valley
+//
+//  Created by Henry Calderon on 3/23/21.
+//
+
+#import "RWGameData.h"
+
+@implementation RWGameData
+
+@end
