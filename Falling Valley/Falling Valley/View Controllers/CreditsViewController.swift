29e6e7a81e72d9fa74dce135fd405a78b34ae1c2
Henry Calderon <theceintiant@gmail.com>
2021-03-22 18:50:01 -0700

    Added a nicer credits page

--- Falling Valley/Falling Valley/View Controllers/CreditsViewController.swift
+++ Falling Valley/Falling Valley/View Controllers/CreditsViewController.swift
@@ -7,17 +7,34 @@
 
 import UIKit
 
-class CreditsViewController: UIViewController {
+class CreditsViewController: UIViewController{
+    
+    let credits: [String] = ["Sound effects obtained from https://www.zapsplat.com","Additional sound effects from https://www.zapsplat.com","Music from https://www.zapsplat.com"]
     
     @IBOutlet weak var exitButton: UIButton!
+    @IBOutlet weak var creditTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        creditTable.delegate = self
+        creditTable.dataSource = self
     }
     
     @IBAction func exitPage(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
     }
-    
+}
 
+extension CreditsViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return credits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "creditCell", for: indexPath)
+        cell.textLabel?.text = credits[indexPath.row]
+        return cell
+    }
+    
+    
 }
